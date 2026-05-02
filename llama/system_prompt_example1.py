8a9066a7108d88e97883c604fc35c88e78f8b7ab
kr-saurabh11 <saurabhcse11@gmail.com>
2026-05-02 21:28:42 +0530

    feat: system prompts

--- llama/system_prompt_example1.py
+++ llama/system_prompt_example1.py
@@ -1,16 +1,16 @@
 from openai import OpenAI
+from openai.types.chat import ChatCompletionSystemMessageParam, ChatCompletionUserMessageParam
 
 system_prompt = """
 You are a math expert. You need to explain as a teacher on how the calculation is working
 """
 
-messages = [
-    {"role": "system", "content":  system_prompt},
+messages: list[ChatCompletionSystemMessageParam | ChatCompletionUserMessageParam] = [
+    {"role": "system", "content": system_prompt},
     {"role": "user", "content": "What is 2 + 2*6//4?"}
 ]
 
 openai = OpenAI(base_url='http://localhost:11434/v1', api_key='ollama')
 response = openai.chat.completions.create(model="llama3.2:1b", messages=messages)
-response.choices[0].message.content
 
 print(response.choices[0].message.content)
--- llama/system_prompt_example2.py
+++ llama/system_prompt_example2.py
@@ -11,6 +11,5 @@ messages = [
 
 openai = OpenAI(base_url='http://localhost:11434/v1', api_key='ollama')
 response = openai.chat.completions.create(model="llama3.2:1b", messages=messages)
-response.choices[0].message.content
 
 print(response.choices[0].message.content)
