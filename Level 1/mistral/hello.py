52c31485c33fcab0c0e817eb6c86b86b5da1eb06
kr-saurabh11 <saurabhcse11@gmail.com>
2026-05-03 12:19:25 +0530

    feat: mistral

---
+++ Level 1/mistral/hello.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv(override=True)
+api_key = os.getenv('OPENAI_API_KEY')
+
+print(f'OPENAI_API_KEY:{api_key}')
+
+message = "Hello, llama! This is my first ever message to you! Hi!"
+messages = [{"role": "user", "content": message}]
+
+openai = OpenAI(base_url='http://localhost:11434/v1', api_key='ollama')
+response = openai.chat.completions.create(model="mistral:7b", messages=messages)  # type: ignore
+
+print(response.choices[0].message.content)
